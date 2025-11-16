3b8c688a9cf65b66b0db25d4125bb73a406bd065
Joe-Zupo <josephmallari.industry.ict@gmail.com>
2025-11-16 12:39:29 +0800

    Updated Book Controller, Added Book Resource

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,56 +3,113 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Http\Resources;
+use App\Http\Resources\BookResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $book = Book::all();
+        $query = Book::query();
+
+        // Filtering
+        if ($request->has('title')) {
+            $query->where('title', 'like', "%" . $request->title . "%");
+        }
+
+        if ($request->has('author')) {
+            $query->where('author', 'like', "%" . $request->author . "%");
+        }
+
+        if ($request->has('year')) {
+            $query->where('year', $request->year);
+        }
+
+        // Sorting
+        if ($request->has('sort')) {
+            $query->orderBy($request->sort, 'asc');
+        }
+
+        // Execute
+        $book = $query->get();
         if($book->isEmpty()){
             return response()->json(['message => No Books Found'], 404);
         }
-        return response()->json($book, 200);
+        return BookResource::collection($book);
     }
 
     public function show($id)
     {
-        $book = Book::findOrFail($id);
-        return response()->json($book, 200);
+        $book = Book::find($id);
+        if(!$book){
+            return response()->json(['message => No Book Found'], 404);
+        }
+        return new BookResource($book, 200);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        $validated = request()->validate([
+        $validated = Validator::make($request->all(),[
             'title' => 'required|string|max:255',
             'author' => 'required|string|max:255',
-            'year' => 'required|integer|digits:4'
+            'year' => 'required|digits:4'
         ]);
 
-        $book = Book::create($validated);
-        return response()->json($book, 201);
+        if($validated->fails()){
+            return response()->json([
+                'message' => 'Fields have wrong input!',
+                'error' => $validated->messages(),
+            ], 422);
+        }
+        $book = Book::create($validated->validated());
+        return response()->json([
+            'message' => 'Book Created Successfuly',
+            'data' => new BookResource($book->fresh())
+        ], 201);
     }
 
-    public function update($id)
+    public function update(Request $request, $id)
     {
-        $validated = request()->validate([
+        //Validated Object
+        $validated = Validator::make($request->all(),[
             'title' => 'sometimes|string|max:255',
             'author' => 'sometimes|string|max:255',
-            'year' => 'sometimes|integer|digits:4'
+            'year' => 'sometimes|digits:4'
         ]);
 
-        $book = Book::findOrFail($id);
-        $book = Book::updated($validated);
+        //VALIDATION
+        if($validated->fails()){
+            return response()->json([
+                'message' => 'Fields have wrong input!',
+                'error' => $validated->messages(),
+            ], 422);
+        }
+        $book = Book::find($id);
+        if(!$book){
+            return response()->json(['message => No Book Found'], 404);
+        }
 
-        return response()->json($book, 200);
+        //assign array data to variable
+        $book->update($validated->validated());
+        return response()->json([
+            'message' => 'Product Updated Successfully!',
+            'data' => new BookResource($book)
+        ], 200);
     }
 
     public function destroy($id)
     {
-        $book = Book::findOrFail($id);
+        $book = Book::find($id);
+        if(!$book){
+            return response()->json(['message => No Book Found'], 404);
+        }
         $book->delete();
-
-        return response()->json(['message' => 'Book Deleted'], 200);
+        return response()->json([
+            'message' => 'Book Deleted',
+            'data' => new BookResource($book)
+        ], 200);
     }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/books', [BookController::class, 'indx']);
+Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/{id}', [BookController::class, 'show']);
 Route::post('/books', [BookController::class, 'store']);
 Route::put('/books/{id}', [BookController::class, 'update']);
